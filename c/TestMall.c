cb2ea9d21f9e97c04ecc7002f6338bc073eba1be
nestor2502 <nestor2502@ciencias.unam.mx>
2019-09-03 18:39:16 -0500

    se sube merge

---
+++ c/TestMall.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+#define SIZE 10000000
+void manejaMatriz(int a[]){
+
+}
+
+int main()
+{
+	int matriz[8];
+	int longitud = sizeof(matriz) / sizeof(matriz[0]);
+	int *a = malloc(8*sizeof(int));
+
+	return 0;
+}
--- c/merge.c
+++ c/merge.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#define SIZE 10000000
 /**
 *Metodo que ordena dos arreglos ordenados
 */
@@ -7,8 +7,13 @@ void merge(int s[], int sub[][2]){
   int arr[8]; //arreglo donde se almacenara el numero ordenado
   int i = sub[0][0]; //inicio sub arreglo 1
   int j = sub[1][0]; //inicio sub arreglo 2
-  int limite_superior1 = sub[0][1]; //tamaño sub arreglo 1
-  int limite_superior2 = sub[1][1]; //tamaño sub arreglo 2
+  int limite_superior1 = sub[0][1]; //cota sup del  sub arreglo 1
+  int limite_superior2 = sub[1][1]; //cota sup del  sub arreglo 2
+  int tamaño1 = sub[0][1]+1;
+  int tamaño2 = sub[1][1]+1-sub[0][1];//tamaño del sub arreglo 2 
+  int *A = malloc(tamaño1*sizeof(int));	
+  //int *B = malloc(tamaño2*sizeof(int);
+  //int *C = malloc(sub[1][1]+1);
   int l = 0; //indice del arreglo ordenado
   while(i<=sub[0][1] && j<=limite_superior2){
     if(s[i]<=s[j] && i<=limite_superior1){
@@ -38,6 +43,7 @@ void merge(int s[], int sub[][2]){
 int main(){
   //int longitud = sizeof(tuArreglo) / sizeof(tuArreglo[0]);
   int matriz[]= {0,2,4,6,1,3,5,7};
+  //int matriz[]= {20, 21, 22, 23, 1, 2, 3, 4};
   int sub [2][2];
   sub[0][0]=0;
   sub[0][1]=3;
@@ -47,5 +53,7 @@ int main(){
   for(int i = 0; i<8; i++){
     printf("%i\n",matriz[i]);
   }
+
   return 0;
 }
+
