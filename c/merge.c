e79cd302c7c6705070ed53afd66341b157deab29
nestor2502 <nestor2502@ciencias.unam.mx>
2019-09-04 18:56:00 -0500

    se termina merge

--- c/merge.c
+++ c/merge.c
@@ -9,41 +9,46 @@ void merge(int s[], int sub[][2]){
   int j = sub[1][0]; //inicio sub arreglo 2
   int limite_superior1 = sub[0][1]; //cota sup del  sub arreglo 1
   int limite_superior2 = sub[1][1]; //cota sup del  sub arreglo 2
-  int tamaño1 = sub[0][1]+1;
-  int tamaño2 = sub[1][1]+1-sub[0][1];//tamaño del sub arreglo 2 
-  int *A = malloc(tamaño1*sizeof(int));	
+  //int tamaño1 = sub[0][1]+1;
+  //int tamaño2 = sub[1][1]+1-sub[0][1];//tamaño del sub arreglo 2 
+  //int *A = malloc(tamaño1*sizeof(int));	
   //int *B = malloc(tamaño2*sizeof(int);
   //int *C = malloc(sub[1][1]+1);
   int l = 0; //indice del arreglo ordenado
-  while(i<=sub[0][1] && j<=limite_superior2){
-    if(s[i]<=s[j] && i<=limite_superior1){
+  while(i<=limite_superior1 && j<=limite_superior2){//me vale verga
+    if(s[i]<=s[j] && i<=limite_superior1){//si el elemento del indice i es menor o igual al indice j
       arr[l]=s[i];
-      l++;
+      l++;//se aumenta el indice del nuevo arreglo
       i++;}
-    else if ( s[j]<=s[i] && j<=limite_superior2 ) {
+    else if ( s[j]<=s[i] && j<=limite_superior2 ) {//si el elemento del indice i es menor o igual al indice j
       arr[l]=s[j];
-      l++;
+      l++;//se aumenta el indice del nuevo arreglo
       j++;}
   }
-  if(i<=limite_superior1){
+
+  if(i<=limite_superior1){//se meten lo numeros del primer intervalo sobrantes 
     for(int r = i; r<=limite_superior1;r++){
       arr[l]=s[i];
+      l++;
+      i++;
     }
   }
-  else if(j<=limite_superior2){
+  else if(j<=limite_superior2){//se meten lo numeros del segundo intervalo sobrantes 
     for(int r = j; r<=limite_superior2;r++){
       arr[l]=s[j];
+      l++;
+      j++;
     }
   }
-  for(int z = 0; z< limite_superior2; z++){
+  for(int z = 0; z<=limite_superior2; z++){
     s[z]= arr[z];}
 }
 
 
 int main(){
   //int longitud = sizeof(tuArreglo) / sizeof(tuArreglo[0]);
-  int matriz[]= {0,2,4,6,1,3,5,7};
-  //int matriz[]= {20, 21, 22, 23, 1, 2, 3, 4};
+  //int matriz[]= {0,2,4,6,1,3,5,7};
+  int matriz[]= {1, 3, 5, 7, 0, 2, 4, 8};
   int sub [2][2];
   sub[0][0]=0;
   sub[0][1]=3;
